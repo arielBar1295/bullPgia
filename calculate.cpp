f69ccd263bb65a5c2af88d0965cb50a055206043
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-04-30 17:11:29 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -12,28 +12,47 @@ string calculateBullAndPgia(string choice,string guess){
     int len=guess.length();
     int b=0;
     int co=0;
-    char g[10];   //putting the guess and the choice into an array.
-    char c[10];
-    bool was[len];
+    char g[10]; //for holding the guess.
+    char c[10]; //for holding the choice.
+    bool was[len];  //an array in order to know whether the specific number is already been checked.
+ //initializing the array .
     for(int i=0;i<len;i++){
         was[i]=false;
     }
+    //removing the guess and choice to arrays.
     strcpy(g,guess.c_str());
     strcpy(c,choice.c_str());
      for(int i=0;i<len;i++){
+         bool secIn=false;
+         int index=0;
         for(int j=0;j<len;j++){
-            if(choice[j]==guess[i]){
-                if(was[j]==false){
-               
-                if(i==j){
+            if(choice[j]==guess[i]) {
+                //for checking if exists another same number in the choice .
+                for (int k = j + 1; k < len; k++) {
+                    if (choice[k] == guess[i] && k == i && was[k] == false) {
+                        secIn = true;
+                        index = k;
+                        was[k] == true;
+                    }
+
+                }
+                if (secIn) {
                     b++;
+                    j = len;
                 }
                 else{
-                    co++;
+                    if (was[j] == false) {
+
+                    if (i == j) {
+                        b++;
+                    } else {
+                        co++;
+                    }
+                    was[j] = true;
+                    j = len;
                 }
-                was[j]=true;
-                 j=len;
             }
+
             }
         }
      }
@@ -41,6 +60,5 @@ string calculateBullAndPgia(string choice,string guess){
      s+=std::to_string(b);
      s+=",";
      s+=std::to_string(co);
-     cout<<s<<endl;
      return s;
  }
