1a6f5c1ccb6fbf202dc31ac5c2a895a589d7a44b
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-04-30 17:09:41 +0300

    Update Guesser.hpp

--- Guesser.hpp
+++ Guesser.hpp
@@ -2,20 +2,37 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
-//#include "calculate.hpp"
-//#include "bullCows.hpp"
+
+
+typedef unsigned int uint;
 using namespace std;
+
  namespace bullpgia{
     class Guesser{
         protected:
         int length;
+        //variables for smatrGuess.
+        string answer; //for knowing the result of the guess.
+        int numIn=0;   //for knowing the times the function has been called.
+        int numLen=0;  //for knowing number of guesses.
+        string previous="";
      public:
      virtual std::string guess(){};
      void startNewGame(int len){
+         if(numIn!=0) {
+             cout << "you made it in " << numIn << " guesses!" << endl;
+         }
          length=len;
+         //every new game, put 0 into those variables.
+         numLen=0;
+         numIn=0;
+         previous="";
      }
      void learn(string b){
-         cout<<"bulls and cows by order : "<<b<<endl;
+         answer=b;
+         if(answer=="4,0"){
+
+         }
      }
     };
 }
