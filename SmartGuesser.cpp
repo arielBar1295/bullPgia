85e442e6375eb2e1cba0f4ec8dbcbaf5942de2a0
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-04-30 17:10:43 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -2,42 +2,160 @@
 #include <stdlib.h>
 #include<stdio.h>
 #include<string.h>
-//#include "play.hpp"
-int n,a[10],b[10],c[10],d[10],B[10],C[10],b1,c1,i,j;
-int numIn=0; //for following the number of guesses.
-string previous ;
-
-
+#include <vector>
+#include <iostream>
+#include <cstddef>
+#include <time.h>
+#include <stddef.h>
+#include <sstream>
+#include "calculate.hpp"
+#include <set>
+#include <list>
+#include <iterator>
+#include<cstdlib>
+#include<ctime>
+//General idea:keeping in a list called possibleAns which holds all possible answers ,starting eliminating the list
+//bu using calculateBullAndPgia.
+//using 5 guesses algorithm by knuth.
+list <string> possibleAns;
 std::string SmartGuesser::guess() {
     numIn++;
-    //first guesse is always a random one.
-    if(numIn==1){
-        string r="";
-        int a1=(rand()%10);
-        char cc='0' + a1;
-        a[0]=a1;
-        r+=cc;
-         int b1=(rand()%10);
-        char c1='0' + b1;
-        b[0]=b1;
-        r+=c1;
-         int c11=(rand()%10);
-        char c2='0' + c11;
-        c[0]=c11;
-        r+=c2;
-         int d1=(rand()%10);
-        char c3='0' + d1;
-        d[0]=d1;
-        r+=c3;
-   
-	return r;
+    if(numIn==1) {
+        //create a randomize first guess.
+        srand(time(0));
+        string first = "";
+        for (int i = 0; i < length; i++) {
+            int r = rand() % 10;
+            first += to_string(r);
+        }
+        previous = first;
+        return first;
     }
-    // else{
-    //  //need to know the result of the previous answer.
-    // }
-    else return "1234";
+    if(this->length==1){
+        numLen++;
+        //building the possibleAnswers only once!
+        if(numLen==1) {
+            possibleAns.clear();
+            for (int d1 = 0; d1 < 10; d1++)
+                    possibleAns.push_back(to_string(d1));
+        }
+        //eliminating
+        for (list<string>::iterator i = possibleAns.begin(); i != possibleAns.end(); i++) {
+            string token = *i;
+            if (calculateBullAndPgia(token, previous) != answer) {
+                possibleAns.remove(*i--);
+            }
+        }
+        //choose randomly from what is left in possibleAns
+        srand( time(0) );
+        int maxRand=possibleAns.size();
+        int r = rand()%maxRand;
+        std::list<string>::iterator it = possibleAns.begin();
+        std::advance(it,r);
+        stringstream ss;
+        ss << *it;
+        string str = ss.str();
+        previous=str;
+        return previous;
 
-    
-   
+    }
+    if(this->length==2) {
+        numLen++;
+        if(numLen==1) {
+            possibleAns.clear();
+            for (int d1 = 0; d1 < 10; d1++) {
+                for (int d2 = 0; d2 < 10; d2++) {
+                        possibleAns.push_back(to_string(d1) + to_string(d2));
+                    }
+            }
+        }
+        for (list<string>::iterator i = possibleAns.begin(); i != possibleAns.end(); i++)
+        {
+            string token =*i;
+            if (calculateBullAndPgia(token,previous) != answer) {
+                possibleAns.remove(*i--);
+            }
+        }
+        srand( time(0) );
+        int maxRand=possibleAns.size();
+        int nextGuess=rand()%maxRand ;
+        std::list<string>::iterator it = possibleAns.begin();
+        std::advance(it,nextGuess);
+        stringstream ss;
+        ss << *it;
+        string str = ss.str();
+        previous=str;
+        return previous;
 
+    }
+    if(this->length==3) {
+        numLen++;
+
+        if(numLen==1) {
+            possibleAns.clear();
+            for (int d1 = 0; d1 < 10; d1++) {
+                for (int d2 = 0; d2 < 10; d2++) {
+                    for (int d3 = 0; d3 < 10; d3++) {
+                        possibleAns.push_back(to_string(d1) + to_string(d2) +
+                                              to_string(d3));
+                    }
+                }
+            }
+        }
+
+        for (list<string>::iterator i = possibleAns.begin(); i != possibleAns.end(); i++)
+        {
+            string token =*i;
+            if (calculateBullAndPgia(token,previous) != answer) {
+                possibleAns.remove(*i--);
+            }
+        }
+        srand( time(0) );
+        int maxRand=possibleAns.size();
+        int nextGuess=rand()%maxRand ;
+        std::list<string>::iterator it = possibleAns.begin();
+        std::advance(it,nextGuess);
+        stringstream ss;
+        ss << *it;
+        string str = ss.str();
+        previous=str;
+        return previous;
+
+    }
+    if(this->length==4 ) {
+        numLen++;
+
+  if(numLen==1) {
+      possibleAns.clear();
+      for (int d1 = 0; d1 < 10; d1++) {
+          for (int d2 = 0; d2 < 10; d2++) {
+              for (int d3 = 0; d3 < 10; d3++) {
+                  for (int d4 = 0; d4 < 10; d4++) {
+                      possibleAns.push_back(to_string(d1) + to_string(d2) +
+                                       to_string(d3) + to_string(d4));
+                  }
+              }
+          }
+      }
+  }
+
+        for (list<string>::iterator i = possibleAns.begin(); i != possibleAns.end(); i++)
+        {
+            string token =*i;
+            if (calculateBullAndPgia(token,previous) != answer) {
+                 possibleAns.remove(*i--);
+            }
+        }
+        srand( time(0) );
+        int maxRand=possibleAns.size();
+        int nextGuess=rand()%maxRand ;
+        std::list<string>::iterator it = possibleAns.begin();
+        std::advance(it,nextGuess);
+        stringstream ss;
+     ss << *it;
+        string str = ss.str();
+        previous=str;
+        return previous;
+
+    }
 }
