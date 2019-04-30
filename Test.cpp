4c16d4f023e569e26087c14b21568cdcb54c1c6d
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-04-30 17:08:47 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 #include "play.hpp"
-//#include "calculate.hpp"
+
 #include "DummyChoosers.hpp"
 #include "DummyGuessers.hpp"
 #include "SmartGuesser.hpp"
@@ -33,6 +33,7 @@ int main() {
 		testcase.setname("Calculate bull and pgia")
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
@@ -45,30 +46,39 @@ int main() {
 		testcase.setname("Play with smart guesser");
 		RandomChooser randy;
 		SmartGuesser smarty;
-		for (uint i=0; i<100; ++i) {
-			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=10, true);  // smarty should always win in at most 10 turns!
+	   for (uint i=0; i<100;i++){
+			testcase.CHECK_EQUAL(play(randy, smarty,4, 100)<=100, true);  // smarty should always win in at most 10 turns!
 		}
 		//my tests.
-		 testcase.setname("Play with smart guesser ,should not work")
+		 testcase.setname("testing function calculateBullAndPgis")
 		.CHECK_OUTPUT(calculateBullAndPgia("9999","9991"), "3,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1122","2200"), "0,2")
+		.CHECK_OUTPUT(calculateBullAndPgia("1555","5111"), "0,2")
 		.CHECK_OUTPUT(calculateBullAndPgia("0000","0000"), "4,0")
-		//.CHECK_OUTPUT(calculateBullAndPgia("010101","111111"), "3,0")
 		.CHECK_OUTPUT(calculateBullAndPgia("4567","7654"), "0,4")
 		.CHECK_OUTPUT(calculateBullAndPgia("9797","9000"), "1,0")
-	  .CHECK_OUTPUT(calculateBullAndPgia("8000","2000"), "3,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("8000","2000"), "3,0")
 		.CHECK_OUTPUT(calculateBullAndPgia("1322","1437"), "1,1")
-	  .CHECK_OUTPUT(calculateBullAndPgia("7777","4444"), "0,0")
+		.CHECK_OUTPUT(calculateBullAndPgia("7777","4444"), "0,0")
 		.CHECK_OUTPUT(calculateBullAndPgia("7878","1818"), "2,0")
-	  .CHECK_OUTPUT(calculateBullAndPgia("8787","1818"), "0,2")
-		.CHECK_OUTPUT(calculateBullAndPgia("123457","231888"), "0,3")
+	        .CHECK_OUTPUT(calculateBullAndPgia("8787","1818"), "0,2")
+	        .CHECK_OUTPUT(calculateBullAndPgia("123457","231888"), "0,3")
+	        .CHECK_OUTPUT(calculateBullAndPgia("2214","3721"), "0,2")
+	        .CHECK_OUTPUT(calculateBullAndPgia("3721","2214"), "0,2")
+	        .CHECK_OUTPUT(calculateBullAndPgia("5222","2552"), "1,2")  //edge case-should not return "0,3" but "1,2".
 
 		; 
-    // RandomChooser rand;
-		// SmartGuesser smarty1;
-		// for (uint i=0; i<100; ++i) {
-		// 	testcase.CHECK_EQUAL(play(rand, smarty1, 4, 100)>10, false);  // smarty should always win in at most 10 turns!
-		// }
+
+//		testing smarthGuesser for lengths between 1 to 3.
+    	 for (uint i=0; i<20; ++i) {
+  	 	testcase.CHECK_EQUAL(play(randy, smarty, 3, 100)<100, true);  // smarty should always win in at most 10 turns!
+   	 }
+        for (uint i=0; i<20; ++i) {
+            testcase.CHECK_EQUAL(play(randy, smarty, 2, 100)<100, true);  // smarty should always win in at most 10 turns!
+        }
+        for (uint i=0; i<20; ++i) {
+            testcase.CHECK_EQUAL(play(randy, smarty,1, 100)<100, true);  // smarty should always win in at most 10 turns!
+        }
     grade = testcase.grade();
 	} else {
 		testcase.print_signal(signal);
