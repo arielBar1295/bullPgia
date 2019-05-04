d2b3567cc11757cce195a65ccfadea36218a1853
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2019-05-04 10:55:22 +0300

    Update calculate.cpp

--- calculate.cpp
+++ calculate.cpp
@@ -9,12 +9,13 @@ string calculateBullAndPgia(string choice,string guess){
     if(choice.length()!=guess.length()){
         throw "Error";
     }
+    cout<<"hello"<<endl;
     int len=guess.length();
     int b=0;
     int co=0;
-    char g[len]; //for holding the guess.
-    char c[len]; //for holding the choice.
-    bool was[len];  //an array in order to know whether the specific number is already been checked.
+    char g[10]; //for holding the guess.
+    char c[10]; //for holding the choice.
+    bool was[10];  //an array in order to know whether the specific number is already been checked.
  //initializing the array .
     for(int i=0;i<len;i++){
         was[i]=false;
@@ -22,23 +23,31 @@ string calculateBullAndPgia(string choice,string guess){
     //removing the guess and choice to arrays.
     strcpy(g,guess.c_str());
     strcpy(c,choice.c_str());
+    bool secIn=false;
+    bool secIn1=false;
      for(int i=0;i<len;i++){
-         bool secIn=false;
-         int index=0;
+         secIn=false;
+         secIn1=false;
         for(int j=0;j<len;j++){
             if(choice[j]==guess[i]) {
                 //for checking if exists another same number in the choice .
-                if(j!=len){
-                for (int k = j + 1; k < len; k++) {
-                    if (choice[k] == guess[i] && k == i && was[k] == false) {
+                for (int k = i + 1; k < len; k++) {
+                    if (choice[j] == guess[k] && k == j && was[j] == false) {
+
                         secIn = true;
-                        index = k;
-                        was[k] = true;
+                        was[j] = true;
                     }
 
                 }
+                for (int k = j + 1; k < len; k++) {
+                    if (choice[k] == guess[i] && k == i && was[j] == false) {
+
+                        secIn1 = true;
+                        was[j] = true;
+                    }
+
                 }
-                if (secIn) {
+                if (secIn||secIn1) {
                     b++;
                     j = len;
                 }
